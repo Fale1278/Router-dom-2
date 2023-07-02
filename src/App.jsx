6e8452b74401a8ef0a8a29355e574117e10774a6
Fale King Nanmua <kingnanmuafale@gmail.com>
2023-07-02 15:18:18 -0700

    Routing

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
+
+const router = createBrowserRouter{
+  createRoutesFromElements(
+    
+  )
+}
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello React.</h1>   
+      <h2>Start editing to see some magic happen!</h2>   
+    </div>
+  )
+}
+
+export default App
