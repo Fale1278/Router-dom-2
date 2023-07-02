db87f2033469f857afb8a5c1c7e9cab5b0f9ee13
Fale King Nanmua <kingnanmuafale@gmail.com>
2023-07-02 15:32:02 -0700

    Routing update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
 import './App.css'
-import { createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, Link, Outlet} from 'react-router-dom'
+import { Home } from './Home.jsx'
+import { Contact } from './Contact.jsx'
+import { Data } from './Data.jsx'
 
-const router = createBrowserRouter{
-  createRoutesFromElements(
-    
+
+function App(props) {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Root />}>
+        <Route index element={<Home />}/>
+        <Route  element={<Data />}/>
+        <Route  element={<Contact />}/>
+      </Route>
+    )
   )
-}
 
-function App() {
-  return (
+  return ( 
     <div className='App'>
       <h1>Hello React.</h1>   
       <h2>Start editing to see some magic happen!</h2>   
     </div>
+  )
+}
+
+const Root = () =>{
+  return(
+    <>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/'>Data</Link>
+      </div>
+
+      <div>
+        <Outlet />
+      </div>
+    </>
   )
 }
 
